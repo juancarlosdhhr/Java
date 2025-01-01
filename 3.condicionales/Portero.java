5a014fad719727c592db025529c6c8ebba942254
juancarlosdhhr <juancarlosdhhr@gmail.com>
2025-01-01 16:48:52 +0000

    update

---
+++ 3.condicionales/Portero.java
@@ -0,0 +1,25 @@
+public class Portero {
+    public static void main(String [] args){
+
+        int edad = 18;
+        System.out.println("Alto ahí, enseñeme el DNI");
+
+
+
+if(edad > 0 && edad < 18) {
+System.out.println("No puedes entrar hasta que cumplas 18 años");
+
+} else if (edad >= 18 && edad <= 60){
+System.out.println("Adelante, disfrute de la velada");
+
+} else if (edad > 60 && edad <= 122){
+System.out.println("Pase usted a la zona con asientos para que no se haga daño");
+
+} else {
+System.out.println("Tira por ahí");
+
+}
+
+
+    }
+}
