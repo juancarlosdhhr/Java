99827f3712c7d5560e654459ba1a44f76db410b9
juancarlosdhhr <juancarlosdhhr@gmail.com>
2025-01-04 17:45:24 +0000

    update

---
+++ bucles/Bucles.java
@@ -0,0 +1,21 @@
+package bucles;
+
+public class Bucles {
+    public static void main(String[] args) {
+
+        //Bucle for
+        /* for (int i = 0; i < 3; i++) {
+            System.out.println("La variable i tiene el valor: " + i);
+        } */
+        //Bucle while
+        int contador = 0;
+
+        while (contador <= 8) {
+            System.out.println("Dentro del bucle " + contador);
+            contador++;
+        }
+
+        System.out.println("Fuera del bucle " + contador);
+    }
+} 
+
