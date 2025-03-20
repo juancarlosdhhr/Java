33c8c22d6ff1bdf25f03e1b5cc0d07b9591f0746
juancarlosdhhr <juancarlosdhhr@gmail.com>
2025-03-20 23:16:16 +0000

    gh

--- VIRTUALLABS/Clase18Metodos.java
+++ VIRTUALLABS/Clase18Metodos.java
@@ -245,6 +245,50 @@ System.out.println("El volumen del cilindro es: " +volumenCilindro(r,h));
 
 //OTRO EJERCICIO
 
+static String decimalBinario(int numeroDecimal){
+
+    String binario= "";
+    int resto;
+
+    if(numeroDecimal == 0){
+        binario = "0";
+
+    }else{
+        //Mientras que el numero decimal sea mayor que 0
+        while(numeroDecimal>0) {
+            resto = numeroDecimal % 2; //Obtiene el resto diviendo por 1 (0/1)
+            binario = resto + binario; //Agrega el resto al inicio de la cadena
+            numeroDecimal = numeroDecimal / 2;
+        }
+
+
+    }
+
+    return binario;
+
+}
+
+
+static int binarioDecimal(String numeroBinario){
+//1010011
+int decimal = 0;
+char digito;
+int multiplicador = 1; // Representa las potencias de 2 (1,2,4,8,...)
+
+for (int = numeroBinario.length()-1;i>0;i--); //1º -> i=6, 2º -> i=5....
+    digito = numeroBinario.charAt(i);
+
+
+    //Si el digito es 1
+    if(digito = '1'){
+        decimal = decimal + multiplicador;  // decimal += multiplicador
+
+//Actualizar el multiplicador (potencia de 2)
+        multiplicador = multiplicador *2; //multiplicador*=2
+    }
+}
+
+
 public static void main (String [] args) {
 
 //Método que convierta un número decimal a binario
